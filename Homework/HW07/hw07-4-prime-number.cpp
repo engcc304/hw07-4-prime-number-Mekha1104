99b597038fead5a642a60c6e377ff228ddf12afb
Mekha1104 <gtrcooie@gmail.com>
2023-08-12 22:45:10 +0700

    07-4

--- Homework/HW07/hw07-4-prime-number.cpp
+++ Homework/HW07/hw07-4-prime-number.cpp
@@ -14,3 +14,39 @@
     Output:
         97 89 83 79 73 71 67 61 59 53 47 43 41 37 31 29 23 19 17 13 11 7 5 3 2
 */
+#include <iostream>
+using namespace std;
+
+bool isPrime(int number) {
+    if (number <= 1) {
+        return false;
+    }
+    for (int i = 2; i <= number / 2; ++i) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    
+    cout << "Enter number :" << endl;
+    cin >> n;
+
+    int i = n;
+
+    cout << "Prime numbers from " << n << " to 2 (in decreasing order):" << endl;
+
+    do {
+        if (isPrime(i)) {
+            cout << i << " ";
+        }
+        --i;
+    } while (i >= 2);
+
+    cout << endl;
+
+    return 0;
+}
